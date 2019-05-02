d46a44f2233db37007e7ab74ab3e031c05b6e677
root <emre.surk@gmail.com>
2019-05-02 16:43:45 +0300

    clear data

---
+++ sources/Helpers/SensitiveDataHelper.js
@@ -0,0 +1,13 @@
+const sensitiveDataHelper = {}
+
+/**
+ * content of that file contains sensitive data, so that file will be added in gitignore.
+ */
+
+const mailCredits = {
+    /**
+     * Empty body. Sensitive data here, will not be pushed...
+     */
+}
+
+module.exports = sensitiveDataHelper;
